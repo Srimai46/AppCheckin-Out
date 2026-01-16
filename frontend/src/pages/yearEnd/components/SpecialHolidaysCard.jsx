9882ecc9cddfa0e8d9f44485a989013cc49c6916
Jokec <bank13946@gmail.com>
2026-01-16 16:06:58 +0700

    fix cancel reason not show in cancellation request

--- frontend/src/pages/yearEnd/components/SpecialHolidaysCard.jsx
+++ frontend/src/pages/yearEnd/components/SpecialHolidaysCard.jsx
@@ -46,7 +46,6 @@ export default function SpecialHolidaysCard() {
     upsertSpecialHoliday,
   } = useHolidayPolicy();
   const { t, i18n } = useTranslation();
-  const emptyHolidayName = { en: "", th: "", ja: "" };
 
 
   
